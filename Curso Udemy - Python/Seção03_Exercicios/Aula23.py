f64f917bef800a8de01580fd9d11aabdbebc7841
Seu Nome <freitascoc687@gmail.com>
2024-07-11 21:48:53 -0300

    Novo curso iniciado.

---
+++ Curso Udemy - Python/Seção03_Exercicios/Aula23.py
@@ -0,0 +1,13 @@
+nome = "Fernandes"
+sobrenome = "De freitas"
+idade = 22
+ano_de_nascimento = 2002
+maior_de_idade = "sim"
+altura_em_metros = 1.83
+
+print = ("Nome: ", nome)
+print = ("Sobrenome: ", sobrenome)
+print = ("idade: ",idade)
+print = ("Ano de nascimento: ", ano_de_nascimento)
+print = ("Maior de idade? ", maior_de_idade)
+print = ("Altura em metros: ", altura_em_metros)
